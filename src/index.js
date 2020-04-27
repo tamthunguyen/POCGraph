8f77e8e70a0e5dc962a4c749cd113be83a67f527
ericyii-cbre <eric.yii@cbre.com>
2020-04-27 09:31:58 -0500

    update on readme and index

--- src/index.js
+++ src/index.js
@@ -2,54 +2,43 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './app/App'
-// import { Provider } from 'react-redux';
-// import { History } from 'history';
-// import { identity } from 'ramda';
 import * as serviceWorker from './serviceWorker';
-// import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-// import { connectRouter, routerMiddleware } from 'connected-react-router';
-// import thunkMiddleware from 'redux-thunk';
-// import { createLogger } from 'redux-logger';
 import Routes from './components/routes';
 
 const createApp = (config, history) => {
-  const initialStore = {
-    entities: {
-      config,
-    },
-  };
-
-
   return (
           <Routes />
   );
 };
 
-const loadApp = (config) => {
-  return createApp();
-};
 
 const renderApp = (elementId, config, basePath) => {
   const el = document.getElementById(elementId);
-  if (!el) {
+  if (!el) { 
     console.error(`failed getting element ${elementId}`);
     return;
   }
 
-  render(loadApp(), el);
+  render(createApp(), el);
 };
 
 class Agency365Search {
   renderSearch = renderApp;
 
   constructor() {
-    // TODO what is this???
+    // check if instance exist in this
     if (!(this instanceof Agency365Search)) {
       return new Agency365Search();
     }
   }
 }
 window.Agency365Search = Agency365Search;
+
+// Main application render
+const ui = <App />
+const container = document.getElementById('root');
+if(container) render(ui, container);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
